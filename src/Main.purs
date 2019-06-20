f990bc880201a08e5baa55dc0468a48f983c774e
eng.ahmed.anwar <eng.ahmed.anwar@gmail.com>
2019-06-21 10:52:56 +1200

    removed redundant functions

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,10 @@
  module Main where
 
-import Prelude
 
-import AddressParser (Address(..), residentialParser)
+import AddressParser (residentialParser)
 import Data.Either (either)
 import Effect (Effect)
+import Prelude
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
@@ -15,23 +15,31 @@ main :: Effect Unit
 main = run [consoleReporter] do
   describe "Address Parser" do
     describe "Valid Address Formats" do
+
         it "Parse \"123 street, city 1234\"" do
             let input = "123 street, city 1234"
             let result = runParser residentialParser input
-            let output = either (\_ -> A2 "" "" "" "") (\x -> x)  $ result
-            output `shouldEqual` A2 "123" "street" "city" "1234"
+            let output = either (\_ -> empty) id  $ result
+            output `shouldEqual` { flatStreet: "123", line1: "street", line2: "", line3: "", line4: "city", postcode: "1234" }
+
         it "Parse \"123/A Long Street, Wide City 1234\"" do
             let input = "123/A Long Street, Wide City 1234"
             let result = runParser residentialParser input
-            let output = either (\_ -> A2 "" "" "" "") (\x -> x)  $ result
-            output `shouldEqual` A2 "123/A" "Long Street" "Wide City" "1234"
+            let output = either (\_ -> empty) id  $ result
+            output `shouldEqual` { flatStreet: "123/A", line1: "Long Street", line2: "", line3: "", line4: "Wide City", postcode: "1234" }        
+
         it "Parse \"123/A Long Street, Fanatasy Suburb, Wide City 1234\"" do
-            let input = "123/A Long Street, Fanatasy Suburb, Wide City 1234"
+            let input = "123/A Long Street, Fantasy Suburb, Wide City 1234"
             let result = runParser residentialParser input
-            let output = either (\_ -> A3 "" "" "" "" "") (\x -> x)  $ result
-            output `shouldEqual` A3 "123/A" "Long Street" "Fanatasy Suburb" "Wide City" "1234"
-        it "Parse \"12/3A Long Street, Fanatasy Suburb, Area, Wide City 1234\"" do
-            let input = "12/3A Long Street, Fanatasy Suburb, Area, Wide City 1234"
+            let output = either (\_ -> empty) id  $ result
+            output `shouldEqual` { flatStreet: "123/A", line1: "Long Street", line2: "Fantasy Suburb", line3: "", line4: "Wide City", postcode: "1234" }        
+
+        it "Parse \"12/3A Long Street, Fantasy Suburb, Area, Wide City 1234\"" do
+            let input = "12/3A Long Street, Fantasy Suburb, Area, Wide City 1234"
             let result = runParser residentialParser input
-            let output = either (\_ -> A4 "" "" "" "" "" "") (\x -> x)  $ result
-            output `shouldEqual` A4 "12/3A" "Long Street" "Fanatasy Suburb" "Area" "Wide City" "1234"
+            let output = either (\_ -> empty) id  $ result
+            output `shouldEqual` { flatStreet: "12/3A", line1: "Long Street", line2: "Fantasy Suburb", line3: "Area", line4: "Wide City", postcode: "1234" }        
+
+    where 
+        empty = { flatStreet: "", line1: "", line2: "", line3: "", line4: "", postcode: "" }
+        id = \x -> x
--- src/parser.purs
+++ src/parser.purs
@@ -5,8 +5,8 @@ import Data.Array (fromFoldable) as A
 import Data.Char.Unicode (isDigit, isLetter)
 import Data.List (List)
 import Data.String.Common (trim)
-import Data.String.Yarn (fromChars, lines)
-import Prelude (class Show, class Eq, Unit, bind, discard, map, pure, void, ($), (<$>), (<<<), (<>), (==), (||), (&&), (/=))
+import Data.String.Yarn (fromChars)
+import Prelude
 import Text.Parsing.StringParser (Parser, try)
 import Text.Parsing.StringParser.CodeUnits (char, oneOf, satisfy)
 import Text.Parsing.StringParser.Combinators (many, many1, sepBy)
@@ -18,39 +18,15 @@ type Line3 = String
 type Line4 = String
 type Postcode = String
 
-data Address =
-    A2 Flat Line1 Line4 Postcode
-  | A3 Flat Line1 Line2 Line4 Postcode
-  | A4 Flat Line1 Line2 Line3 Line4 Postcode
-  | Invalid
-
-data ResultAddress = ResultAddress {
+type Address =
+    {
         flatStreet :: String
     ,   line1 :: String
     ,   line2 :: String
     ,   line3 :: String
     ,   line4 :: String
     ,   postcode :: String
-  }
-
-instance showAddress :: Show Address where
-    show (A2 f l1 l4 p) = "A2 [" <> f <> "|" <> l1 <> "|"  <> l4 <> "|" <> p <> "]"
-    show (A3 f l1 l2 l3 p) = "A3 [" <> f <> "|" <> l1 <> "|" <> l2 <> "|" <> l3 <> "|" <> p <> "]"
-    show (A4 f l1 l2 l3 l4 p ) = "A4 [" <> f <> "|" <> l1 <> "|" <> l2 <> "|" <> l3 <> "|" <> l4 <> "|" <> p <> "]"
-    show (Invalid) = "Invalid"
-
-instance showResultAddress :: Show ResultAddress where
-    show (ResultAddress { flatStreet: f, line1:l1, line2:l2, line3:l3, line4:l4, postcode:p }) =
-        "ResultAddress [" <> f <> "|" <> l1 <> "|" <> l2 <> "|" <> l3 <> "|" <> l4 <> "|" <> p <> "]"
-
-instance eqAddress :: Eq Address where
-    eq (A4 f l1 l2 l3 l4 p) (A4 f' l1' l2' l3' l4' p') = 
-        f == f' && l1 == l1' && l2 == l2' && l3 == l3' && l4 == l4' && p == p'
-    eq (A3 f l1 l2 l4 p) (A3 f' l1' l2' l4' p') = 
-        f == f' && l1 == l1' && l2 == l2' && l4 == l4' && p == p'
-    eq (A2 f l1 l4 p) (A2 f' l1' l4' p') = 
-        f == f' && l1 == l1' && l4 == l4' && p == p'
-    eq _ _ = true
+    }
 
 digits :: Parser (List Char)
 digits = many $ satisfy $ isDigit
@@ -84,7 +60,7 @@ sepBySpace = toString <$> (many1 $ satisfy $ \c ->  c /= ',')   `sepBy` char ' '
 
 addressParserA2 :: Parser Address
 addressParserA2 = do
-    flat      <- word
+    f      <- try word <|> try poBox
     skipWhitespaces 
     l1        <- addressLine 
     skipComma
@@ -92,15 +68,19 @@ addressParserA2 = do
     l4        <- addressLine
     skipWhitespaces 
     p        <- postcode
-    pure $ A2 
-        flat 
-        l1 
-        (trim l4)
-        p
+    pure $
+        {
+            flatStreet: f
+        ,   line1: l1
+        ,   line2: ""
+        ,   line3: ""
+        ,   line4: (trim l4)
+        ,   postcode: p
+        } 
 
 addressParserA3 :: Parser Address
 addressParserA3 = do
-    flat      <- word
+    flat      <- try word <|> try poBox
     skipWhitespaces 
     l1        <- addressLine 
     skipComma
@@ -111,16 +91,19 @@ addressParserA3 = do
     l4        <- addressLine
     skipWhitespaces 
     p        <- postcode
-    pure $ A3
-        flat 
-        l1
-        l2
-        (trim l4)
-        p 
+    pure $
+        {
+            flatStreet: flat
+        ,   line1: l1
+        ,   line2: l2
+        ,   line3: ""
+        ,   line4: (trim l4)
+        ,   postcode: p
+        } 
 
 addressParserA4 :: Parser Address
 addressParserA4 = do
-    flat      <- word
+    flat      <- try word <|> try poBox
     skipWhitespaces 
     l1        <- addressLine 
     skipComma
@@ -134,89 +117,21 @@ addressParserA4 = do
     l4        <- addressLine
     skipWhitespaces 
     p        <- postcode
-    pure $ A4
-        flat 
-        l1
-        l2
-        l3
-        (trim l4)
-        p 
-
-
-addressPostalParserA2 :: Parser Address
-addressPostalParserA2 = do
-    l1        <- poBox 
-    skipComma
-    skipWhitespaces
-    l4        <- addressLine
-    skipWhitespaces 
-    p        <- postcode
-    pure $ A2 
-        "" 
-        l1 
-        (trim l4)
-        p
-
-addressPostalParserA3 :: Parser Address
-addressPostalParserA3 = do
-    l1        <- poBox 
-    skipComma
-    skipWhitespaces
-    l2        <- addressLine 
-    skipComma
-    skipWhitespaces
-    l4        <- addressLine
-    skipWhitespaces 
-    p        <- postcode
-    pure $ A3
-        "" 
-        l1 
-        l2
-        (trim l4)
-        p
-
-addressPostalParserA4 :: Parser Address
-addressPostalParserA4 = do
-    l1        <- poBox 
-    skipComma
-    skipWhitespaces
-    l2        <- addressLine 
-    skipComma
-    skipWhitespaces
-    l3        <- addressLine 
-    skipComma
-    skipWhitespaces
-    l4        <- addressLine
-    skipWhitespaces 
-    p        <- postcode
-    pure $ A4
-        "" 
-        l1 
-        l2
-        l3
-        (trim l4)
-        p
+    pure $
+        {
+            flatStreet: flat
+        ,   line1: l1
+        ,   line2: l2
+        ,   line3: l3
+        ,   line4: (trim l4)
+        ,   postcode: p
+        } 
 
 residentialParser :: Parser Address
 residentialParser = 
         try addressParserA2
     <|> try addressParserA3 
     <|> try addressParserA4
-    <|> try addressPostalParserA2
-    <|> try addressPostalParserA3
-    <|> try addressPostalParserA4
-
-
-
-mkResultAddress :: Address -> ResultAddress
-mkResultAddress (A2 f l1 l4 p) = 
-    ResultAddress { flatStreet: f, line1: l1, line2: "", line3: "", line4: l4, postcode: p }
-mkResultAddress (A3 f l1 l2 l4 p) = 
-    ResultAddress { flatStreet: f, line1: l1, line2: l2, line3: "", line4: l4, postcode: p }
-mkResultAddress (A4 f l1 l2 l3 l4 p) = 
-    ResultAddress { flatStreet: f, line1: l1, line2: l2, line3: l3, line4: l4, postcode: p }
-mkResultAddress (Invalid) = 
-    ResultAddress { flatStreet: "", line1: "", line2: "", line3: "", line4: "", postcode: "" }
 
 i4 :: String
 i4 = "123 line, line, line, line 1234"
